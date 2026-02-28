e13f2ac1f51930cd19aae100ae57e0785f76c971
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-02-28 15:42:49 +0900

    feat: textlint用の設定を追加

--- dot_config/nvim/lua/plugins/format.lua
+++ dot_config/nvim/lua/plugins/format.lua
@@ -7,6 +7,7 @@ return {
         ["javascriptreact"] = { "biome-check" },
         ["typescript"] = { "biome-check" },
         ["typescriptreact"] = { "biome-check" },
+        ["Markdown"] = { "textlint" },
         ["json"] = { "biome-check" },
         ["css"] = { "biome-check" },
         ["python"] = {
@@ -17,6 +18,28 @@ return {
           -- To organize the imports.
           "ruff_organize_imports",
         },
+      }
+      opts.formatters = {
+        textlint = {
+          meta = {
+            url = "https://github.com/textlint/textlint",
+            description = "The pluggable natural language linter for text and markdown.",
+          },
+          command = require("conform.util").from_node_modules("textlint"),
+          stdin = true,
+          args = {
+            "--fix",
+            "--stdin",
+            "--stdin-filename",
+            "$FILENAME",
+            "--format",
+            "fixed-result",
+            "--dry-run",
+          },
+          cwd = require("conform.util").root_file({
+            "package.json",
+          })
+        }
       }
       return opts
     end,
