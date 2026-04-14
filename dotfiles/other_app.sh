8f1a81d49e4ce790548a322df0335789ff20add5
labfish <168062620+labFish00@users.noreply.github.com>
2026-04-14 15:22:08 +0900

    feat: nixで管理できないものをother_app.shに管理

---
+++ dotfiles/other_app.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install snapd flatpak obs-studio steam-installer qemu-system-x86 libvirt-daemon-system libvirt-clients bridge-utils virt-manager -y
+flatpak install flathub com.google.Chrome
+flatpak install flathub com.spotify.Client
+snap install discord
+curl -fsSL https://tailscale.com/install.sh | sh
