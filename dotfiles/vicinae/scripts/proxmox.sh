7895fd1b0c107f0e84ead30a1ff6df1c3c7128e9
labfish <168062620+labFish00@users.noreply.github.com>
2026-04-24 14:13:11 +0900

    feat: proxmoxのショートカットを追加

---
+++ dotfiles/vicinae/scripts/proxmox.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title 🔗 Proxmox Virtual Environment - boxfish
+# @vicinae.mode silent
+# @vicinae.icon https://192.168.68.10:8006/pve2/images/logo-128.png
+# @vicinae.description boxfish - Proxmox Virtual Environment のページを開きます
+
+xdg-open "https://192.168.68.10:8006/"
