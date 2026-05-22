84fb5704db1498877b5a467e30fc8be2c6256f1a
boxfish <79849824+boxfish-jp@users.noreply.github.com>
2026-05-22 20:01:17 +0900

    feat: vicinaeにtwitchを追加

---
+++ dotfiles/vicinae/scripts/twitch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title 🔗 Twitch
+# @vicinae.mode silent
+# @vicinae.icon https://assets.twitch.tv/assets/favicon-32-e29e246c157142c94346.png
+# @vicinae.description Twitch のページを開きます
+
+xdg-open "https://www.twitch.tv/"
