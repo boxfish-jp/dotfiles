5ce38580d1911ffcdcd53d82c1e7a79724b1e027
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-06-08 19:10:24 +0900

    fix: サスペンド後にクラッシュする問題を修正

--- configuration.nix
+++ configuration.nix
@@ -122,7 +122,7 @@
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    open = true;
+    open = false;
     modesetting.enable = true;
     powerManagement.enable = true;
   };
