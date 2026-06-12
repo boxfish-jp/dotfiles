89ed0c20b759ebf51cc5c18d2dec7cb5c5200d59
labfish <168062620+labFish00@users.noreply.github.com>
2026-06-12 16:58:27 +0900

    feat: 貸せ、tree-sitterはこうやる

--- nvim/default.nix
+++ nvim/default.nix
@@ -5,28 +5,6 @@
   ...
 }:
 let
-  grammars = with pkgs.tree-sitter-grammars; [
-    tree-sitter-lua
-    tree-sitter-python
-    tree-sitter-typescript
-    tree-sitter-nix
-  ];
-  mkParserEntry = pkg: {
-    name = "nvim/site/parser/${lib.removePrefix "tree-sitter-" pkg.pname}.so";
-    value = {
-      source = "${pkg}/parser";
-    };
-  };
-
-  mkQueriesEntry = pkg: {
-    name = "nvim/site/queries/${lib.removePrefix "tree-sitter-" pkg.pname}";
-    value = {
-      source = "${pkg}/queries";
-    };
-  };
-
-  allEntries = map mkParserEntry grammars ++ map mkQueriesEntry grammars;
-
   vimdoc-ja = pkgs.vimUtils.buildVimPlugin {
     name = "vim-easygrep";
     src = pkgs.fetchFromGitHub {
@@ -60,10 +38,12 @@ in
       lualine-nvim
       grug-far-nvim
       gitsigns-nvim
+      (nvim-treesitter.withPlugins (p: with p; [
+        tree-sitter-python
+        tree-sitter-nix
+      ]))
     ];
   };
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dotfiles/nvim";
-
-  xdg.dataFile = lib.listToAttrs allEntries;
 }
