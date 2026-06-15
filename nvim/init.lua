df9d4188d2be0e62af49c7de7c798a173146e110
labfish <168062620+labFish00@users.noreply.github.com>
2026-06-15 14:07:03 +0900

    fix: vim.loader.enableを前に持ってこないと不安定っぽい

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,10 @@
 vim.g.startup_time = vim.loop.now()
-local function gh(repo)
-  return "https://github.com/" .. repo
-end
+vim.loader.enable()
 do
   require("options")
   require("globals")
   require("mappings")
   require("plugins.init")
-  vim.loader.enable()
   vim.schedule(function()
     vim.o.clipboard = "unnamedplus"
   end)
