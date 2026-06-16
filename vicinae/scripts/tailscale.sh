cdafbdd65346e4b779e63a65f76262b3210f35e5
labfish <168062620+labFish00@users.noreply.github.com>
2026-06-17 00:07:42 +0900

    feat: tailscaleの起動スクリプトを追加

---
+++ vicinae/scripts/tailscale.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title 🔗 Tailscale | Secure Connectivity for AI,
+# @vicinae.mode silent
+# @vicinae.icon https://tailscale.com/favicon.png
+# @vicinae.description Tailscale | Secure Connectivity for AI,  のページを開きます
+
+xdg-open "https://login.tailscale.com/admin/machines"
