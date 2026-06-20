c438c2825ecc8e13a579e73f8b03c49395699b42
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-06-21 00:39:55 +0900

    feat: プロジェクトに依存している開発ツールを削除

--- common-modules/default.nix
+++ common-modules/default.nix
@@ -7,35 +7,24 @@
 {
   home.packages = with pkgs; [
     git
-    curl
     lazygit
     fzf
     ripgrep
     fd
     wl-clipboard
-    gnumake
-    pkg-config
-    clang
     hackgen-nf-font
     starship
-    pnpm
-    uv
     gh
     ffmpeg
-    gdb
-    cmake
     yt-dlp
     discord
     spotify
     ncdu
     vlc
-    cargo
-    rustc
     krita
     vscode
     google-chrome
     podman-compose
-    nodejs_24
     kdePackages.krohnkite
   ];
 }
