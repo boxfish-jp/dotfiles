d09bef4c792e1b0700e19da95bfb262db3c054b9
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-07-02 01:31:25 +0900

    feat: tailscaleをserverに追加

--- hosts/server/home.nix
+++ hosts/server/home.nix
@@ -27,6 +27,7 @@
 
     streaming-kit-voicevox-connector = {
       enable = true;
+      voicevoxUrls = "http://boxfish-1.taildb6ca.ts.net:50021,http://127.0.0.1:50021";
       systemd.enable = true;
     };
   };
