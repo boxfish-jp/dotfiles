594db5f86d7a6ede8cc6287feee01768d976a794
labfish <168062620+labFish00@users.noreply.github.com>
2026-07-08 17:05:20 +0900

    feat: tldrawのショートカットを追加

---
+++ vicinae/scripts/tldraw.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title 🔗 tldraw • very good free whiteboard
+# @vicinae.mode silent
+# @vicinae.icon https://www.tldraw.com/favicon.svg
+# @vicinae.description tldraw • very good free whiteboard のページを開きます
+
+xdg-open "https://www.tldraw.com/"
