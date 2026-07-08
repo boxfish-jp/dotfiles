a4d1a59965d7db44aabd15f4e5ce0f47c09e1c36
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-07-08 23:27:52 +0900

    feat: vicinaeスクリプトにサーバー系のアプリを追加

---
+++ vicinae/scripts/adguard_home.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title 🔗 AdGuard Home
+# @vicinae.mode silent
+# @vicinae.icon http://192.168.68.16:4003/assets/apple-touch-icon-180x180.png
+# @vicinae.description AdGuard Home のページを開きます
+
+xdg-open "http://192.168.68.16:4003/#"
---
+++ vicinae/scripts/audiobookshelf.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# @vicinae.schemaVersion 1
+# @vicinae.title 🔗 Audiobookshelf
+# @vicinae.mode silent
+# @vicinae.icon http://192.168.68.16:13378/audiobookshelf/favicon.ico
+# @vicinae.description Audiobookshelf のページを開きます
+
+xdg-open "http://192.168.68.16:13378/"
