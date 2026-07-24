edc06edb7ec37386a110636619b5e841f1178cc7
labfish <168062620+labFish00@users.noreply.github.com>
2026-07-24 13:19:10 +0900

    feat: kimakiをsystemdで管理するコードを追加

--- llm/default.nix
+++ llm/default.nix
@@ -6,14 +6,11 @@
   ...
 }:
 {
-  home.packages =
-    with pkgs;
-    lib.optionals (username != "server") [
-      (pkgs.callPackage ./kimaki { })
-    ]
-    ++ [
-      llm-agents.opencode
-    ];
+  imports = lib.optionals (username == "server") [ ./kimaki ];
+
+  home.packages = with pkgs; [
+    llm-agents.opencode
+  ];
 
   xdg.configFile."opencode".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/dotfiles/llm/opencode";
