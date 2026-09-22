50af153da2dbdc0b4630e9d983f7120f4356829f
boxfish_jp <79849824+boxfish-jp@users.noreply.github.com>
2026-09-23 00:30:53 +0900

    refactor: zellijをパッケージ化した

--- zellij/default.nix
+++ zellij/default.nix
@@ -1,18 +1,30 @@
-{ self, ... }:
+{ lib, ... }:
+let
+  # zellij 設定ツリー（config.kdl と themes/ のみ）
+  zellijConfigTree = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./config.kdl
+      ./themes
+    ];
+  };
+in
 {
-  flake.homeModules.zellij =
+  perSystem =
+    { pkgs, ... }:
     {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
-    {
-      home.packages = [
-        pkgs.zellij
-      ];
-      xdg.configFile."zellij".source = ./.;
-    }
+      packages.zellij = pkgs.writeShellApplication {
+        name = "zellij";
+        runtimeInputs = [ pkgs.zellij ];
+        runtimeEnv.ZELLIJ_CONFIG_DIR = zellijConfigTree;
+        text = ''zellij "$@"'';
+      };
+    };
 
-  ;
+  flake.homeModules.zellij =
+    { config, pkgs, ... }:
+    {
+      home.packages = [ pkgs.zellij ];
+      xdg.configFile."zellij".source = ./.;
+    };
 }
